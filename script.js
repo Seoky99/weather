343a2c5b68c8a2b82d278562a09eeba0534c9288
Seoky99 <seokyoung99@gmail.com>
2025-04-10 11:20:41 -0400

    catching 400 HTTP, displaying error

--- script.js
+++ script.js
@@ -3,24 +3,13 @@ async function submitRequest(searchQuery) {
         const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${searchQuery}?key=23TQH9KDEMJ6FHV2TD6WZDNBN`,
             {mode: 'cors'});
         if (!response.ok) {
-            throw new Error(`Response status: ${response.status}`);
+            throw new Error(`${response.status}`);
         } 
         return response.json();
     } catch (error) {
         throw new Error(error.message);
     }
 }
-
-/*async function test() {
-    try {
-        console.log(await submitRequest("Londo2asddasn"));
-        console.log("success");
-    } catch(err) {
-        console.log(err);
-    }
-}
-
-test().catch(() => {console.log("success!!")}); */
 
 async function adjustDOM(searchQuery) {
     const data = await submitRequest(searchQuery); 
@@ -33,18 +22,30 @@ async function adjustDOM(searchQuery) {
 }
 
 function handleButtons() {
-    const form = document.querySelector("form");
     const input = document.querySelector("#search-input");
+    const submitButton = document.querySelector("form button");
 
     const test = document.querySelector("#test");
-    form.addEventListener("submit", async (e) => {
+    submitButton.addEventListener("click", async (e) => {
         e.preventDefault();
+        input.setCustomValidity("");
+
+        if (!(input.validity.valid)) {
+            input.reportValidity(); 
+            return; 
+        }
+
         try {
             await adjustDOM(input.value);
         } catch (err) {
-            test.textContent = err;
-            console.log("here!");
-        }
+            if (err.message === "400") {
+                input.setCustomValidity("Please type a valid location.");
+                input.reportValidity();
+            } else {
+                test.textContent = "Sorry! Server error";
+            }
+
+        } 
     }); 
 } 
 
